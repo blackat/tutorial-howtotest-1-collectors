e2f90d4b77d37ef8fc23e8d1a89ea0599b18ff79
blackat <eugenio_lentini@fastmail.fm>
2015-06-21 09:40:33 +0200

    added basic test classes

---
+++ exercise-to-be-corrected/src/test/java/com/contrastofbeauty/tutorial/collectors/TweetCollectorTest.java
@@ -0,0 +1,24 @@
+package com.contrastofbeauty.tutorial.collectors;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TweetCollectorTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testAccept() throws Exception {
+
+    }
+
+    @Test
+    public void testFlush() throws Exception {
+
+    }
+}
---
+++ exercise-to-be-corrected/src/test/java/com/contrastofbeauty/tutorial/services/CloudServiceTest.java
@@ -0,0 +1,19 @@
+package com.contrastofbeauty.tutorial.services;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CloudServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testAddCollector() throws Exception {
+
+    }
+}
